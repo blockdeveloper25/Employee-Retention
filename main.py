4b96523fab81af28d9d3a74ebc46f646990120ba
blockdeveloper25 <sujairblockdev@gmail.com>
2025-04-29 17:09:38 +0530

    model finding with feature engineering

--- main.py
+++ main.py
@@ -1,21 +1,71 @@
-from flask import Flask, render_template, request
-from flask import Response, jsonify
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response
+import flask_monitoringdashboard as dashboard
+import pandas as pd
+import os
 from flask_cors import CORS, cross_origin
+from apps.training.train_model import TrainModel
+from apps.prediction.predict_model import PredictModel
+from apps.core.config import Config
+"""
+*****************************************************************************
+*
+* filename:       main.py
+* version:        1.0
+* author:         CODESTUDIO
+* creation date:  28-APRIL-2025
+*
+* change history:
+*
+* who             when           version  change (include bug# if apply)
+* ----------      -----------    -------  ------------------------------
+* Sujair          28-APRIL-2025    1.0      initial creation
+*
+*
+* description:    flask main file to run application
+*
+****************************************************************************
+"""
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/training', methods=['POST'])
+
+@app.route('/training', methods=['POST']) 
 @cross_origin()
 def training_route_client():
+    """
+    * method: training_route_client
+    * description: method to call training route
+    * return: none
+    *
+    * who             when           version  change (include bug# if apply)
+    * ----------      -----------    -------  ------------------------------
+    * Sujair          28-APRIL-2025    1.0      initial creation
+    *
+    * Parameters
+    *   None
+    """
     try:
-        return Response("Training started", status=200)
+        config = Config()
+        #get run id
+        run_id = config.get_run_id()
+        data_path = config.training_data_path
+        #trainmodel object initialization
+        trainModel=TrainModel(run_id,data_path)
+        #training the model
+        trainModel.training_model()
+        return Response("Training successfull! and its RunID is : "+str(run_id))
     except ValueError:
-        return Response("Error Occured! %s" % str(ValueError), status=500)
+        return Response("Error Occurred! %s" % ValueError)
     except KeyError:
         return Response("Error Occurred! %s" % KeyError)
     except Exception as e:
         return Response("Error Occurred! %s" % e)
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    #app.run(debug=True)
+    host = '0.0.0.0'
+    port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    httpd.serve_forever()
